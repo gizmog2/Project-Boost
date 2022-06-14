ca1944e779c4bd6503e6d476c117504b9be87355
Сергей Буркун <GizmoG2@live.ru>
2022-06-14 18:42:29 +0300

    Добавлен скрипт для управления игроком.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotate right");
+        }
+    }
+
+    private void ProcessTrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Spase is pressed.");
+        }
+        
+        
+    }
+}
