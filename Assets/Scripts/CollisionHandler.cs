0904a47d3e4cc8b4f5b369180a2ea29ca5dc6aa8
Сергей Буркун <GizmoG2@live.ru>
2022-06-20 16:48:02 +0300

    Сделана загрузка этапа в зависимости от взаимодейси от ситуации.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -17,12 +17,32 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("You get fuel");
                 break;
             case "Finish":
-                Debug.Log("You finished");
+                NextLevel();                
                 break;
             default:
                 ReloadLevel();
                 break;
         }
+    }
+
+    private void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextLevelIndex = currentSceneIndex + 1;
+        if (nextLevelIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextLevelIndex = 0;
+        }
+        SceneManager.LoadScene(nextLevelIndex);
+        /*if (nextLevelIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextLevelIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }*/
+
     }
 
     private void ReloadLevel()
